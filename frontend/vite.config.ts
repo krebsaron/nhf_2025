f13419e1ca874afd0bee346c2ef4dd64e3a13179
Marton Mack <mackmarton@edu.bme.hu>
2025-11-24 22:41:29 +0100

    Update ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8082',
         changeOrigin: true,
       },
       '/ws': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8082',
         changeOrigin: true,
         ws: true,
       },
